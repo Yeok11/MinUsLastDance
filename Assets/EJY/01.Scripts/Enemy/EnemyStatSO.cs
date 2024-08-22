77fe0ad47391faca3395d43575da4b3987ef8469
arke0909 <arke0909@gmail.com>
2024-08-22 18:44:49 +0900

    Enemy

---
+++ Assets/EJY/01.Scripts/Enemy/EnemyStatSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Enemy/StatData")]
+public class EnemyStatSO : ScriptableObject
+{
+    public float _hp;
+    public float _attack;
+    public float _barrier;
+    public int _targets;
+}
