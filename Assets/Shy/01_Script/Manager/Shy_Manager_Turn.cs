37d572a0288e711d8a5bc33f9bacf5cbb0f78f75
sin2gami <sin2gami@naver.com>
2024-08-28 15:54:05 +0700

    d

--- Assets/Shy/01_Script/Manager/Shy_Manager_Turn.cs
+++ Assets/Shy/01_Script/Manager/Shy_Manager_Turn.cs
@@ -2,15 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shy_Enemy
-{
-
-}
-
 public class Shy_Manager_Turn : MonoBehaviour
 {
     public List<GameObject> turnList;
-    public List<Shy_Enemy> enemys;
+    //public List<Enemy> enemys;
 
     
 }
