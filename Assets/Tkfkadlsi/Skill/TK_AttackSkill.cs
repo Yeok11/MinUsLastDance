94e5c3c167ae501c21f888ae4d63c7e1f1316e82
tkfkadlsi <yeseonggim35@gmail.com>
2024-09-03 00:23:59 +0900

    스킬 일단 만들었는데.
    스킬들 스크립트 파서 종류에 맞게 상속 받고 (Buff, Attack, Healing) public override void UseSkill(Shy_Player player, List<EJY_Enemy> targets) 만들어야돼.
    
    구체적인 구현할때 타겟 리스트에 직접 스킬별로 타겟 지정, 공격 스킬이면 데미지 계산 (뭐 플레이어 스탯 같은거 들고와서 어쩌구 저쩌구 한 다음에 데미지 변수에 넣어놔)
    
    그리고 공격 스킬이면 UseSkill 마지막에 Attack()실행하세요.

--- Assets/Tkfkadlsi/Skill/TK_AttackSkill.cs
+++ Assets/Tkfkadlsi/Skill/TK_AttackSkill.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TK_AttackSkill : TK_Skill
 {
+    [SerializeField] protected float damage;
     protected List<EJY_Enemy> targets = new List<EJY_Enemy>();
 
-    public override bool UseSkill(Shy_Player player)
+    public override bool CanUseSkill(Shy_Player player)
     {
-        return base.UseSkill(player);
+        return base.CanUseSkill(player);
     }
 
     public override void SkillLevelUp()
     {
         base.SkillLevelUp();
+    }
+
+    public override void UseSkill(Shy_Player player, List<EJY_Enemy> targets)
+    {
+        base.UseSkill(player, targets);
+    }
+
+    protected void Attack()
+    {
+        targets.ForEach(e =>
+        {
+            e.HealthCompo.TakeDamage(damage);
+        });
     }
 }
--- Assets/Tkfkadlsi/Skill/TK_HealingSkill.cs
+++ Assets/Tkfkadlsi/Skill/TK_HealingSkill.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class TK_HealingSkill : TK_Skill
 {
-    public override bool UseSkill(Shy_Player player)
+    public override bool CanUseSkill(Shy_Player player)
     {
-        return base.UseSkill(player);
+        return base.CanUseSkill(player);
     }
 
     public override void SkillLevelUp()
     {
         base.SkillLevelUp();
+    }
+
+    public override void UseSkill(Shy_Player player, List<EJY_Enemy> targets)
+    {
+        base.UseSkill(player, targets);
     }
 }
