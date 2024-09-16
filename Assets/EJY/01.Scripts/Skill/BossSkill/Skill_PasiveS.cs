c524b26f8b8c5c4458466adbeb65e13b485002cb
Yeok11 <sin2gami@naver.com>
2024-09-16 12:42:29 +0700

    bug resolve

--- Assets/EJY/01.Scripts/Skill/BossSkill/Skill_PasiveS.cs
+++ Assets/EJY/01.Scripts/Skill/BossSkill/Skill_PasiveS.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 namespace EJY
 {
-<<<<<<<< HEAD:Assets/EJY/01.Scripts/Skill/BossSkill/Skill_PasiveS.cs
     public class Skill_PasiveS : Skill_Stack
-========
-    public class Skill_BarrierElectro : Skill_Stack
->>>>>>>> origin/EJY:Assets/EJY/01.Scripts/Skill/BossSkill/ElectroFrog/Skill_BarrierElectro.cs
     {
         protected float weightBarrier;
         public override void Awake()
@@ -23,16 +19,13 @@ namespace EJY
 
         public override void UseSkill()
         {
-<<<<<<<< HEAD:Assets/EJY/01.Scripts/Skill/BossSkill/Skill_PasiveS.cs
             target.stacks.Add(stackPrefab.Init(target.transform.GetChild(0)));
-========
             target.stacks.Add(stackPrefab);
 
             //방어력 계산
             weightBarrier = 30 + (target.HealthCompo._maxHp - target.HealthCompo._currentHp) * 0.15f; 
 
             target.HealthCompo.GetBarrier(weightBarrier);
->>>>>>>> origin/EJY:Assets/EJY/01.Scripts/Skill/BossSkill/ElectroFrog/Skill_BarrierElectro.cs
         }
     }
 }
