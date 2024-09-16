8a46f4dddc9dc51a645bab8c4e7d25c73fc800f2
Yeok11 <sin2gami@naver.com>
2024-09-16 12:44:49 +0700

    a

--- Assets/EJY/01.Scripts/Skill/Debuffer/Debuffer_Passive.cs
+++ Assets/EJY/01.Scripts/Skill/Debuffer/Debuffer_Passive.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 namespace EJY
 {
-<<<<<<<< HEAD:Assets/EJY/01.Scripts/Skill/BossSkill/ElectroFrog/Skill_BarrierElectro.cs
-    public class Skill_BarrierElectro : Skill_Stack
-========
     public class Debuffer_Passive : Skill_Stack
->>>>>>>> 41d503ae26f0e3bce19bedac156644e1834db085:Assets/EJY/01.Scripts/Skill/Debuffer/Debuffer_Passive.cs
     {
         protected float weightBarrier;
         public override void Awake()
@@ -23,16 +19,14 @@ namespace EJY
 
         public override void UseSkill()
         {
-<<<<<<<< HEAD:Assets/EJY/01.Scripts/Skill/BossSkill/ElectroFrog/Skill_BarrierElectro.cs
-            target.stacks.Add(stackPrefab);
+            target.stacks.Add(stackPrefab.Init(target.transform.GetChild(0)));
 
             //방어력 계산
             weightBarrier = 30 + (target.HealthCompo._maxHp - target.HealthCompo._currentHp) * 0.15f; 
 
             target.HealthCompo.GetBarrier(weightBarrier);
-========
-            target.stacks.Add(stackPrefab.Init(target.transform.GetChild(0)));
->>>>>>>> 41d503ae26f0e3bce19bedac156644e1834db085:Assets/EJY/01.Scripts/Skill/Debuffer/Debuffer_Passive.cs
+
+            
         }
     }
 
