a155a10e45e42f7cb8105537638e7672f4d042e3
Yeok11 <sin2gami@naver.com>
2024-09-24 07:47:29 +0700

    uiUpdate

---
+++ Assets/EJY/01.Scripts/Enemy/Enemise/TestEnemy_Shy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEnemy_Shy : EJY_Enemy
+{
+    protected override void EnemyAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Shy/01_Script/Manager/Shy_Manager_Move.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Shy_Manager_Move : MonoBehaviour
+{
+    [Header("행동력")]
+    public int actionPoint;
+    public int ActionPoint
+    {
+        get => actionPoint;
+        set
+        {
+            actionPoint = maxActionPoint < value ? maxActionPoint : value;
+            actionPoint = value >= 0 ? value : 0;
+        }
+    }
+    public int maxActionPoint;
+
+    [Header("이동력")]
+    public int movePoint = 0;
+
+    [Header("텍스트")]
+    [SerializeField] private TextMeshProUGUI text_actionPoint;
+    [SerializeField] private TextMeshProUGUI text_movePoint;
+    private Shy_Player player;
+
+
+
+    private void Awake()
+    {
+        player = transform.parent.GetComponentInChildren<Shy_Player>();
+        ActionPoint = maxActionPoint;
+    }
+
+    public void Update_PointSign()
+    {
+        text_actionPoint.SetText(ActionPoint + " / " + maxActionPoint);
+        text_movePoint.SetText("MP : " + movePoint);
+    }
+
+    private void Update()
+    {
+        Update_PointSign();
+    }
+}
