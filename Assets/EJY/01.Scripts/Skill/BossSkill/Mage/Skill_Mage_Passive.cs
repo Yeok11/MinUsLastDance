d4bf6d23640ad0aca7563bc62c993a24a5ca7859
arke0909 <arke0909@gmail.com>
2024-09-24 10:02:24 +0900

    메이지_스크립트만

---
+++ Assets/EJY/01.Scripts/Skill/BossSkill/Mage/Skill_Mage_Passive.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EJY
+{
+    public class Skill_Mage_Passive : Skill_Stack
+    {
+        public override bool CanUseSkill()
+        {
+            return true;
+        }
+
+        public override void UseSkill()
+        {
+            target.stacks.Add(stackPrefab.Init(target.transform.GetChild(0)));
+        }
+    }
+}
