5315ed98c57d834895e955b5dc83a7929ae62ba5
Yeok11 <sin2gami@naver.com>
2024-10-01 22:20:02 +0700

    bugSet

--- Assets/djEJsdlfma/01.Scripts/Skills/EJD_PowerSlash.cs
+++ Assets/djEJsdlfma/01.Scripts/Skills/EJD_PowerSlash.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ah_Skill_Aiming : TK_AttackSkill
+public class EJD_PowerSlash : TK_AttackSkill
 {
     //야성 스택을 n만큼 얻습니다, 이번 전투동안 덱에 조준을 하나 추가합니다.
     //N = 2 + (이번 턴에 사용한 사냥 타일의 수 * 0.5) 강화 불가 
